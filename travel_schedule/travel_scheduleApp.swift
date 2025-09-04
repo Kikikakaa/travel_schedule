d4bc702c18eaa8d9834d9c43d75343b210689fd2
Никита <uzer591@gmail.com>
2025-09-05 02:25:31 +0500

    Initial Commit

---
+++ travel_schedule/travel_scheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  travel_scheduleApp.swift
+//  travel_schedule
+//
+//  Created by user on 05.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct travel_scheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ travel_scheduleTests/travel_scheduleTests.swift
@@ -0,0 +1,17 @@
+//
+//  travel_scheduleTests.swift
+//  travel_scheduleTests
+//
+//  Created by user on 05.09.2025.
+//
+
+import Testing
+@testable import travel_schedule
+
+struct travel_scheduleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
